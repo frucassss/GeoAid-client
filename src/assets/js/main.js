8eb5914054d1c2bab61715d3d208798706c9cd70
Lucas Guillemyn <lucas.guillemyn@student.howest.be>
2022-11-08 15:05:30 +0100

    basic loading animation when you load page

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,7 +5,9 @@ document.addEventListener("DOMContentLoaded", loadConfig);
 // Entry point for the JS code.
 function init() {
     // Very small proof of concept.
-    poc();
+    //poc();
+    console.log("hi");
+    setTimeout(startupAnimaiton, 2500);
 }
 
 function loadConfig() {
@@ -15,4 +17,12 @@ function loadConfig() {
             api = `${config.host ? config.host + '/': ''}${config.group ? config.group + '/' : ''}api/`;
             init();
         });
+}
+
+function startupAnimaiton() {
+    const loader = document.querySelector(".loading");
+    const h1 = document.querySelector("h1");
+    loader.classList.add("hidden");
+    h1.classList.remove("hidden");
+    h1.classList.add("lineup");
 }
