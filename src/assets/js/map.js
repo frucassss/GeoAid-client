4e93f4383e4185d47dc0c2b041f836e4bada4481
senne.vandecasteele <senne.vandecasteele@student.howest.be>
2022-11-09 14:03:21 +0100

    added custom icons

--- src/assets/js/map.js
+++ src/assets/js/map.js
@@ -1,15 +1,29 @@
-
     //common consts
     const mbUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
     const mbAttr = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> '
    
+    
 
     //needs some code cleanup
    function createMap() {
+    //icons
+    const domeIcon = L.Icon.extend({
+        options: {
+            iconSize: [40, 40]
+        }
+    });
+    const dangerIcon = L.Icon.extend({
+        options: {
+            iconSize: [50, 50]
+        }
+    });
+    
+    const blackdome = new domeIcon({iconUrl: 'assets/images/blackdome.png'});
+    const danger = new dangerIcon({iconUrl: 'assets/images/danger.png'});
     //domes marker
     const domes = L.layerGroup();
-    const dome1 = L.marker([36, -110.5]).bindPopup('This is Dome1').addTo(domes);
-    const dome2 = L.marker([38.74, -104]).bindPopup('This is Dome2.').addTo(domes);
+    const dome1 = L.marker([36, -110.5], {icon: blackdome}).bindPopup('This is Dome1').addTo(domes);
+    const dome2 = L.marker([38.74, -104], {icon: blackdome}).bindPopup('This is Dome2.').addTo(domes);
 
     const osm = L.tileLayer(mbUrl, {
         attribution: mbAttr
@@ -30,7 +44,7 @@
     const layerControl = L.control.layers(baseLayers, overlays).addTo(map);
 
         //dangers
-        const crime = L.marker([40.74, -120]).bindPopup('Crime')
+        const crime = L.marker([40.74, -120], {icon: danger}).bindPopup('Crime')
 
         const dangers = L.layerGroup([crime]);
         layerControl.addOverlay(dangers, 'Dangers');
