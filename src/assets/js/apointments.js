8d44cd2bd58aa62d0f5dca058dfa920c03e05847
senne.vandecasteele <senne.vandecasteele@student.howest.be>
2022-12-08 15:33:13 +0100

    added subject

--- src/assets/js/apointments.js
+++ src/assets/js/apointments.js
@@ -2,19 +2,22 @@ const btn = document.getElementById("submitbtn");
 const article = document.getElementById("appointment-list");
 const appointments = document.getElementById("appointments");
 let appointmentNumber = 1;
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function (e) {
   btn.addEventListener("click", submit);
 });
 
-function submit() {
+function submit(e) {
+  e.preventDefault();
   article.classList.remove("hidden");
   let date = document.querySelector("#date").value;
   let time = document.querySelector("#time").value;
   let expertise = document.querySelector("#expertise").value;
   let employee = document.querySelector("#employee").value;
+  let subject = document.querySelector("#subject").value;
 
   appointments.innerHTML += `<div>
     <h3>Appointment ${appointmentNumber++}</h3>
+    <h3>Subject: ${subject}</h3>
     <p>Date: ${date}</p>
     <p>Hour: ${time}</p>
     <p>Expertise: ${expertise}</p>
