0593cafc896de17949a0f9705c68cfdae4b594a7
senne.vandecasteele <senne.vandecasteele@student.howest.be>
2022-12-19 09:56:25 +0100

    all help buttons added

--- src/assets/js/company.js
+++ src/assets/js/company.js
@@ -1,17 +1,19 @@
-import {createLineChart} from "./modules/graphs.js";
-import {selectClickedCategory} from "./modules/helper.js";
+import { createLineChart } from "./modules/graphs.js";
+import { selectClickedCategory } from "./modules/helper.js";
+import { eventListenerPopup } from "./modules/popup.js";
 
 function init() {
-    handleEventListeners();
-    createLineChart();
+  handleEventListeners();
+  createLineChart();
+  eventListenerPopup();
 }
 
 function handleEventListeners() {
-    selectClickedCategory("#company-chart h4", createLineChart);
+  selectClickedCategory("#company-chart h4", createLineChart);
 
-    document.querySelectorAll("#years input").forEach(year => {
-        year.addEventListener("change", createLineChart)
-    })
+  document.querySelectorAll("#years input").forEach((year) => {
+    year.addEventListener("change", createLineChart);
+  });
 }
 
-init()
+init();
