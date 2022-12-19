7b821c5a644ed7d8a3726c597a7a02649c148dc4
Loesje <jarne.losschaert@student.howest.be>
2022-12-19 11:32:01 +0100

    one popup done

--- src/assets/js/modules/popup.js
+++ src/assets/js/modules/popup.js
@@ -1,21 +1,33 @@
 export function eventListenerPopup() {
     document.querySelectorAll(".help").forEach(help => {
-        help.addEventListener("click", showPopup)
+        help.addEventListener("click", showPopup);
     });
 
     document.querySelectorAll(".popup .close").forEach(close => {
-        close.addEventListener("click", hidePopup)
+        close.addEventListener("click", hidePopup);
     })
 }
 
 function showPopup(e) {
     const popupId = e.target.dataset.help;
     const $popup = document.querySelector(`#popup-${popupId}`);
-    $popup.classList.remove("hidden")
+    $popup.classList.remove("hidden");
+    addBlur();
 }
 
 function hidePopup(e) {
     const $target = e.target;
     const $popup = $target.closest(".popup");
-    $popup.classList.add("hidden")
+    $popup.classList.add("hidden");
+    removeBlur();
+}
+
+function addBlur() {
+    const $body = document.querySelector("body");
+    $body.classList.add("blur");
+}
+
+function removeBlur() {
+    const $body = document.querySelector("body");
+    $body.classList.remove("blur");
 }
