e0a9a9cd8ce38d988d427363aca10de62d5536ce
senne.vandecasteele <senne.vandecasteele@student.howest.be>
2022-12-19 09:51:21 +0100

    help in stats done, needs 1 styling detail

--- src/assets/js/statistics.js
+++ src/assets/js/statistics.js
@@ -1,83 +1,87 @@
-import {makeSuggestions, selectClickedCategory} from "./modules/helper.js";
-import {createBarChart, createPieChart} from "./modules/graphs.js";
-import {get, setApi} from "./modules/api.js";
+import { makeSuggestions, selectClickedCategory } from "./modules/helper.js";
+import { createBarChart, createPieChart } from "./modules/graphs.js";
+import { get, setApi } from "./modules/api.js";
+import { eventListenerPopup } from "./modules/popup.js";
 
 const PIECHARTTITLES = {
-    crimes: "Type of crimes",
-    oxygen_leaks: "Danger level of oxygen leaks",
-    population: "Jobs of population",
-    medical_dispaches: "Danger level of medical dispaches"
-}
+  crimes: "Type of crimes",
+  oxygen_leaks: "Danger level of oxygen leaks",
+  population: "Jobs of population",
+  medical_dispaches: "Danger level of medical dispaches",
+};
 
 function loadConfig() {
-    fetch("config.json")
-        .then(resp => resp.json())
-        .then(config => {
-            const api = `${config.host ? config.host + '/': ''}${config.group ? config.group + '/' : ''}api/`;
-            setApi(api);
-            init();
-        });
+  fetch("config.json")
+    .then((resp) => resp.json())
+    .then((config) => {
+      const api = `${config.host ? config.host + "/" : ""}${
+        config.group ? config.group + "/" : ""
+      }api/`;
+      setApi(api);
+      init();
+    });
 }
 function init() {
-    handleEventListeners();
-    defaultDome();
-    defaultSuggestions();
-    createBarChart();
-    createPieChart();
+  handleEventListeners();
+  defaultDome();
+  defaultSuggestions();
+  createBarChart();
+  createPieChart();
+  eventListenerPopup();
 }
 
 function handleEventListeners() {
-    selectClickedCategory("#category_chart h2", function () {
-        makeCharts();
-        const $target = document.querySelector("#types_chart h2");
-        let category = document.querySelector("aside .selected").id;
-        category = category.replace("-","_");
-        $target.innerText = PIECHARTTITLES[category];
+  selectClickedCategory("#category_chart h2", function () {
+    makeCharts();
+    const $target = document.querySelector("#types_chart h2");
+    let category = document.querySelector("aside .selected").id;
+    category = category.replace("-", "_");
+    $target.innerText = PIECHARTTITLES[category];
+  });
+
+  document
+    .querySelector("#searchbar input")
+    .addEventListener("keyup", function () {
+      makeSuggestions(addEventListenersSuggestions);
     });
 
-    document.querySelector('#searchbar input').addEventListener("keyup", function () {
-        makeSuggestions(addEventListenersSuggestions);
-    });
+  document.querySelector("#period").addEventListener("change", makeCharts);
 
-    document.querySelector("#period").addEventListener("change", makeCharts)
-
-    function makeCharts() {
-        createBarChart();
-        createPieChart();
-    }
+  function makeCharts() {
+    createBarChart();
+    createPieChart();
+  }
 }
 
-
-
 function addEventListenersSuggestions() {
-    document.querySelectorAll('#suggestions li').forEach(li => {
-        li.addEventListener("click", function (ev) {
-            const $clicked = ev.currentTarget;
-            const id = $clicked.id;
-            const domeName = $clicked.querySelector("p").innerText;
-            const $target = document.querySelector("#dome-choice h3");
-            $target.innerText = domeName;
-            $target.id = id;
+  document.querySelectorAll("#suggestions li").forEach((li) => {
+    li.addEventListener("click", function (ev) {
+      const $clicked = ev.currentTarget;
+      const id = $clicked.id;
+      const domeName = $clicked.querySelector("p").innerText;
+      const $target = document.querySelector("#dome-choice h3");
+      $target.innerText = domeName;
+      $target.id = id;
 
-            createPieChart();
-        })
-    })
+      createPieChart();
+    });
+  });
 }
 
 function defaultDome() {
-    get("domes", succesHandler);
+  get("domes", succesHandler);
 
-    function succesHandler(res) {
-        (res.json()).then((data) => {
-            const $target = document.querySelector("#dome-choice h3");
-            const domeName = data.domes[0].domeName;
-            $target.innerText = domeName;
-        });
-    }
+  function succesHandler(res) {
+    res.json().then((data) => {
+      const $target = document.querySelector("#dome-choice h3");
+      const domeName = data.domes[0].domeName;
+      $target.innerText = domeName;
+    });
+  }
 }
 
 function defaultSuggestions() {
-    makeSuggestions(addEventListenersSuggestions);
+  makeSuggestions(addEventListenersSuggestions);
 }
 
 loadConfig();
