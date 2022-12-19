b6731d20c3ec9ac767a8c7dd41feddf2332a1da2
Loesje <jarne.losschaert@student.howest.be>
2022-12-19 18:32:47 +0100

    appointments done

--- src/assets/js/profile.js
+++ src/assets/js/profile.js
@@ -1,37 +1,67 @@
 import {setColorScheme} from "./modules/helper.js";
 import {eventListenerPopup} from "./modules/popup.js";
+import {get, post, setApi} from "./modules/api.js";
+
+function loadConfig() {
+  fetch("config.json")
+      .then((resp) => resp.json())
+      .then((config) => {
+        const api = `${config.host ? config.host + "/" : ""}${config.group ? config.group + "/" : ""}api/`;
+        setApi(api);
+        init();
+      });
+}
 
 function init() {
   handleEventListeners();
+  displayAppointments();
   setColorScheme();
   eventListenerPopup();
 }
 
 function handleEventListeners() {
-  document.getElementById("submitbtn").addEventListener("click", submit);
+  document.querySelector("form").addEventListener("submit", submitAppointment);
 }
 
-const article = document.getElementById("appointment-list");
-const appointments = document.getElementById("appointments");
-let appointmentNumber = 1;
+function displayAppointments() {
+  const $target = document.querySelector("#appointments");
+  $target.innerHTML = "";
 
-function submit(e) {
+  get("appointments", succesHandler)
+  function succesHandler(res) {
+    res.json().then(data => {
+      const appointments = data.appointments;
+      appointments.forEach(appointment => {
+        const clone = document.querySelector("#clone-appointment").cloneNode(true);
+        clone.classList.remove("hidden")
+        clone.querySelector(".subject").innerText = appointment.topic;
+        clone.querySelector(".date").innerText = appointment.date;
+        clone.querySelector(".time").innerText = appointment.time;
+        clone.querySelector(".expertise").innerText = appointment.expertise;
+        clone.querySelector(".employee").innerText = appointment.employeeId;
+        $target.appendChild(clone);
+      })
+    });
+  }
+}
+
+function submitAppointment(e) {
   e.preventDefault();
-  article.classList.remove("hidden");
-  let date = document.querySelector("#date").value;
-  let time = document.querySelector("#time").value;
-  let expertise = document.querySelector("#expertise").value;
-  let employee = document.querySelector("#employee").value;
-  let subject = document.querySelector("#subject").value;
+  const date = document.querySelector("#date").value;
+  const time = document.querySelector("#time").value;
+  const expertise = document.querySelector("#expertise").value;
+  const employee = document.querySelector("#employee").value;
+  const subject = document.querySelector("#subject").value;
 
-  appointments.innerHTML += `<div>
-    <h3>Appointment ${appointmentNumber++}</h3>
-    <h3>Subject: ${subject}</h3>
-    <p>Date: ${date}</p>
-    <p>Hour: ${time}</p>
-    <p>Expertise: ${expertise}</p>
-    <p>Employee: ${employee}</p>
-    </div> `;
+  const body = {
+    "date": date,
+    "time": time,
+    "topic": subject,
+    "employee_id": 1,
+    "expertise": expertise
+  };
+
+  post("appointment", body, displayAppointments);
 }
 
-init();
+loadConfig();
